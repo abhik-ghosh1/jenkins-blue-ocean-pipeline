ab0c2ea3fdd7e9b49e7d41ae7d86cc8709140455
abhik <abhik.ghosh84@outlook.com>
2019-08-06 00:33:58 +0530

    First commit

---
+++ src/main/java/com/t/ThisClass.java
@@ -0,0 +1,10 @@
+package com.t;
+
+public class ThisClass {
+
+	public static void main(String[] args) {
+		System.out.println("This is just a sample program");
+
+	}
+
+}
---
+++ src/test/java/testjenkins/TestThisClass.java
@@ -0,0 +1,14 @@
+package testjenkins;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestThisClass {
+
+	@Test
+	public void test() {
+		System.out.println("This is a Test Class for ThisClass");
+	}
+
+}
